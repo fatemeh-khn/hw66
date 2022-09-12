5d2704d23cf7658eff7e521a5ee33cb5b6c65188
fatemeh_kahen <kahen1368@gmail.com>
2022-09-12 09:11:01 -0700

    add property for match class and team class

---
+++ src/main/java/entities/Match.java
@@ -0,0 +1,11 @@
+package entities;
+
+import java.util.Date;
+
+public class Match {
+    private Team first;
+    private Team second;
+    private int teamFirstScore;
+    private int teamSecondScore;
+    private Date date;
+}
